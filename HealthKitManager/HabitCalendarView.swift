12d0f55ae40e59d6b3a99fbb6eeaa4c491fd138f
jgaehler <jgaehler@enloc.de>
2025-12-25 22:28:38 +0100

    Update 2026

---
+++ HealthKitManager/HabitCalendarView.swift
@@ -0,0 +1,18 @@
+//
+//  HabitCalendarView.swift
+//  HealthKitManager
+//
+//  Created by Jan Gähler on 15.07.25.
+//
+
+import SwiftUI
+
+struct HabitCalendarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HabitCalendarView()
+}
